695cd013ac8339278f3743e382023f41ea2252f7
wang89531734 <89531734@qq.com>
2019-11-18 18:03:47 +0800

    11.18

---
+++ HHRPG/Assets/Scripts/JyGame/MapEvent.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace JyGame
+{
+	// Token: 0x02000051 RID: 81
+	[XmlType("event")]
+	public class MapEvent
+	{
+		// Token: 0x17000087 RID: 135
+		// (get) Token: 0x06000210 RID: 528 RVA: 0x0001815C File Offset: 0x0001635C
+		[XmlIgnore]
+		public int probability
+		{
+			get
+			{
+				int? num = this.probabilityValue;
+				int result;
+				if (num == null)
+				{
+					result = 100;
+				}
+				else
+				{
+					int? num2 = this.probabilityValue;
+					result = num2.Value;
+				}
+				return result;
+			}
+		}
+
+		// Token: 0x17000088 RID: 136
+		// (get) Token: 0x06000211 RID: 529 RVA: 0x00018194 File Offset: 0x00016394
+		public bool IsRepeatOnce
+		{
+			get
+			{
+				return this.repeatValue == "once";
+			}
+		}
+
+		// Token: 0x17000089 RID: 137
+		// (get) Token: 0x06000212 RID: 530 RVA: 0x000181A8 File Offset: 0x000163A8
+		[XmlIgnore]
+		public bool IsActive
+		{
+			get
+			{
+				if (this.IsRepeatOnce && RuntimeData.Instance.IsStoryFinished(this.value))
+				{
+					return false;
+				}
+				if (!Tools.ProbabilityTest((double)this.probability / 100.0))
+				{
+					return false;
+				}
+				foreach (Condition condition in this.Conditions)
+				{
+					if (!condition.IsTrue)
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+
+		// Token: 0x040001C0 RID: 448
+		[XmlAttribute]
+		public string type;
+
+		// Token: 0x040001C1 RID: 449
+		[XmlAttribute]
+		public string value;
+
+		// Token: 0x040001C2 RID: 450
+		[XmlAttribute]
+		public string image;
+
+		// Token: 0x040001C3 RID: 451
+		[XmlAttribute("probability")]
+		public int? probabilityValue;
+
+		// Token: 0x040001C4 RID: 452
+		[XmlAttribute]
+		public int lv = -1;
+
+		// Token: 0x040001C5 RID: 453
+		[XmlAttribute]
+		public string description;
+
+		// Token: 0x040001C6 RID: 454
+		[XmlAttribute("repeat")]
+		public string repeatValue;
+
+		// Token: 0x040001C7 RID: 455
+		[XmlElement("condition")]
+		public List<Condition> Conditions;
+	}
+}
---
+++ HHRPG/Assets/Scripts/JyGame/MapRole.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace JyGame
+{
+	// Token: 0x02000052 RID: 82
+	[XmlType("maprole")]
+	public class MapRole
+	{
+		// Token: 0x06000214 RID: 532 RVA: 0x00018264 File Offset: 0x00016464
+		//public MapEvent GetActiveEvent()
+		//{
+		//	foreach (MapEvent mapEvent in this.Events)
+		//	{
+		//		if (mapEvent.IsActive)
+		//		{
+		//			return mapEvent;
+		//		}
+		//	}
+		//	return null;
+		//}
+
+		// Token: 0x1700008A RID: 138
+		// (get) Token: 0x06000215 RID: 533 RVA: 0x000182D8 File Offset: 0x000164D8
+		//[XmlIgnore]
+		//public bool IsActive
+		//{
+		//	get
+		//	{
+		//		return this.GetActiveEvent() != null;
+		//	}
+		//}
+
+		// Token: 0x1700008B RID: 139
+		// (get) Token: 0x06000216 RID: 534 RVA: 0x000182E8 File Offset: 0x000164E8
+		// (set) Token: 0x06000217 RID: 535 RVA: 0x00018330 File Offset: 0x00016530
+		[XmlIgnore]
+		public string Name
+		{
+			get
+			{
+				Role role = ResourceManager.Get<Role>(this.name);
+				if (role != null)
+				{
+					//return CommonSettings.getRoleName(this.name);
+				}
+				return this.name.TrimEnd(new char[]
+				{
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6',
+					'7',
+					'8',
+					'9',
+					'0'
+				});
+			}
+			set
+			{
+				this.name = value;
+			}
+		}
+
+		// Token: 0x1700008C RID: 140
+		// (get) Token: 0x06000218 RID: 536 RVA: 0x0001833C File Offset: 0x0001653C
+		[XmlIgnore]
+		public string roleKey
+		{
+			get
+			{
+				return this.name;
+			}
+		}
+
+		// Token: 0x040001C8 RID: 456
+		[XmlAttribute]
+		public string description;
+
+		// Token: 0x040001C9 RID: 457
+		[XmlElement("event")]
+		//public List<MapEvent> Events;
+
+		// Token: 0x040001CA RID: 458
+		[XmlAttribute]
+		public string pic;
+
+		// Token: 0x040001CB RID: 459
+		[XmlAttribute]
+		public string name;
+	}
+}
